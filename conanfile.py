5145d4535029d260e4a44da269d370fbdaf85a55
Ian Murfin <ianmurfin@protonmail.com>
2022-03-20 07:40:43 +0000

    package info fixes

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class SDLMixerConan(ConanFile):
 		postfix = "d" if build_type == "Debug" else ""
 		
 		if self.settings.os == "Windows":
-			static = "-static" if self.options.shared else ""
+			static = "-static" if not self.options.shared else ""
 			self.cpp_info.libs = [
 				f"SDL_mixer{static}{postfix}.lib"
 			]
